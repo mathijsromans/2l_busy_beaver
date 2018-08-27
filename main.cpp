0975fed230f3f6b349ce77a49eaf0c38a8db5514
Mathijs Romans <github@romansland.nl>
2018-08-28 01:11:21 +0200

    Remove exception to optimize

--- main.cpp
+++ main.cpp
@@ -183,9 +183,7 @@ Field read_file(std::string const& filename)
     return f;
 }
 
-class ExitException : public std::exception {};
-
-void move(int dir, int& px, int& py)
+void move(int dir, int& px, int& py, bool& out_of_bounds)
 {
     switch (dir) {
         case 0: /* up */
@@ -202,9 +200,7 @@ void move(int dir, int& px, int& py)
             break;
     }
     /* quit if < 0 */
-    if (px < 0 || py < 0) {
-        throw ExitException();
-    }
+    out_of_bounds = px < 0 || py < 0;
 }
 
 unsigned int execute(Field const& f, char *outbuf, unsigned int max_steps, unsigned int dlev)
@@ -213,85 +209,93 @@ unsigned int execute(Field const& f, char *outbuf, unsigned int max_steps, unsig
     int py = -1;
     int dir = 2;
 
-    char mbuf[32256];
-    memset(mbuf, 0, 32256);
-    int mloc = 32256/2;
+    const int mem_size = 128;
+    char mbuf[mem_size];
+    memset(mbuf, 0, mem_size);
+    int mloc = mem_size/2;
 
     unsigned int steps = 0;
-    try {
-        while(true) {
-            int next_x = px;
-            int next_y = py;
-            move(dir, next_x, next_y);
-            while(f.get(next_x, next_y) == '+') {
-                if (mbuf[mloc]) {
-                    dir = (dir+1)%4; // turn right
-                } else {
-                    dir = (dir+3)%4; // turn left
-                }
-                next_x = px;
-                next_y = py;
-                move(dir, next_x, next_y);
+    while(true) {
+        int next_x = px;
+        int next_y = py;
+        bool out_of_bounds = false;
+        move(dir, next_x, next_y, out_of_bounds);
+        if (out_of_bounds) {
+            return steps;
+        }
+        while(f.get(next_x, next_y) == '+') {
+            if (mbuf[mloc]) {
+                dir = (dir+1)%4; // turn right
+            } else {
+                dir = (dir+3)%4; // turn left
             }
-            px = next_x;
-            py = next_y;
-            ++steps;
-            if (f.get(px, py) == '*') {
-                if (mloc == 1 && (dir == 1 || dir == 3)) {
-                    /* IO */
-                    if (mbuf[0] == 0) { /* input */
-                        mbuf[0] = static_cast<char>(getchar());
-                    } else { /* output */
-                        /* if debugging, buffer output */
-                        if (dlev != 0) {
-                            sprintf(outbuf+strlen(outbuf), "%c", mbuf[0]);
-                        } else {
-                            putchar(mbuf[0]);
-                            fflush(stdout);
-                        }
-                        mbuf[0] = 0;
-                    }
-                } else { /* not IO */
-                    switch (dir) {
-                        case 0: /* up */
-                            mloc--;
-                            break;
-                        case 1: /* right */
-                            mbuf[mloc]++;
-                            break;
-                        case 2: /* down */
-                            mloc++;
-                            break;
-                        case 3: /* left */
-                            mbuf[mloc]--;
-                            break;
-                    }
-                }
-            }
-
-            /* if debugging, output */
-            if (dlev != 0) {
-                f.print(px, py);
-                fflush(stdout);
-                printf("%d\n\n", steps);
-                usleep(1000000 / dlev);
-            }
-
-            if (steps > max_steps) {
-                return 0;
+            next_x = px;
+            next_y = py;
+            move(dir, next_x, next_y, out_of_bounds);
+            if (out_of_bounds) {
+                return steps;
             }
         }
-    } catch (ExitException const&) {
-        return steps;
+        px = next_x;
+        py = next_y;
+        ++steps;
+        if (f.get(px, py) == '*') {
+//            if (mloc == 1 && (dir == 1 || dir == 3)) {
+//                /* IO */
+//                if (mbuf[0] == 0) { /* input */
+//                    mbuf[0] = static_cast<char>(getchar());
+//                } else { /* output */
+//                    /* if debugging, buffer output */
+//                    if (dlev != 0) {
+//                        sprintf(outbuf+strlen(outbuf), "%c", mbuf[0]);
+//                    } else {
+//                        putchar(mbuf[0]);
+//                        fflush(stdout);
+//                    }
+//                    mbuf[0] = 0;
+//                }
+//            } else { /* not IO */
+                switch (dir) {
+                    case 0: /* up */
+                        mloc--;
+                        break;
+                    case 1: /* right */
+                        mbuf[mloc]++;
+                        break;
+                    case 2: /* down */
+                        mloc++;
+                        break;
+                    case 3: /* left */
+                        mbuf[mloc]--;
+                        break;
+                }
+                if (mloc < 0 || mloc >= mem_size)
+                {
+                    return steps;
+                }
+//            }
+        }
+
+        /* if debugging, output */
+        if (dlev != 0) {
+            f.print(px, py);
+            fflush(stdout);
+            printf("%d\n\n", steps);
+            usleep(1000000 / dlev);
+        }
+
+        if (steps > max_steps) {
+            return 0;
+        }
     }
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        printf("Use: %s <program>\n", argv[0]);
-        return 1;
-    }
+//    if (argc < 2) {
+//        printf("Use: %s <program>\n", argv[0]);
+//        return 1;
+//    }
 
     char *outbuf = nullptr;
     unsigned int dlev;
@@ -309,7 +313,7 @@ int main(int argc, char **argv)
 //    execute(ff, outbuf, 100, 10);
 //    return 0;
 
-    const unsigned int size = 5;
+    const unsigned int size = 6;
     Field orig = first_field(size);
     Field f = orig;
     Field best_field = orig;
@@ -318,7 +322,7 @@ int main(int argc, char **argv)
     do
     {
 //        f.print();
-        unsigned int steps = execute(f, outbuf, 100, 0);
+        unsigned int steps = execute(f, outbuf, 400, 0);
 //        printf("Total steps: %d\n", steps);
         if ( steps > max_steps ) {
             max_steps = steps;
@@ -327,7 +331,7 @@ int main(int argc, char **argv)
 
         f = next(f);
         ++iter;
-        if (iter % 100 == 0) {
+        if (iter % 10000 == 0) {
             const unsigned int total_steps = pow(3, (size-1)*(size-1));
             printf("iter = %d / %d (%g%%)\n", iter, total_steps, 100.0 * iter / total_steps);
             fflush(stdout);
