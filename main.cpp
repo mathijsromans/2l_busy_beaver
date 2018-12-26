9b2ba1f576d191ee1e665772ab8eb760fee0fe8a
Mathijs Romans <github@romansland.nl>
2018-12-26 14:46:44 +0100

    Ready for presentation

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ constexpr unsigned long powr(unsigned long a, unsigned long b)
 
 void test_next()
 {
-    const unsigned int SIZE = 3;
+    const unsigned int SIZE = 5;
     Field<SIZE> orig = first_field<SIZE>();
 
     Field<SIZE> a = orig;
@@ -39,7 +39,7 @@ void test_next()
 
 int main()
 {
-    const int SIZE = 1;
+    const int SIZE = 7;
     unsigned long iter = 0;
     unsigned long iter_start = 0;
     unsigned int max_steps = 0;
@@ -47,11 +47,12 @@ int main()
     Field<SIZE> best_field = orig;
     Field<SIZE> f = orig;
     std::vector<Field<SIZE>> error_fields;
+    unsigned int num_error_fields = 0;
     if (!filename.empty()) {
         f = read_file<SIZE>(filename);
         Run<SIZE> r;
         r.reset(f);
-        Run<SIZE>::Result result = r.execute(100000);
+        Run<SIZE>::Result result = r.execute(1000000);
         if (result.type == Run<SIZE>::ResultType::error) {
             std::cout << "Field could not be evaluated: " << std::endl; }
         else if ( result.type == Run<SIZE>::ResultType::finite && result.steps > max_steps ) {
@@ -64,11 +65,12 @@ int main()
     do
     {
         r.reset(f);
-        Run<SIZE>::Result result = r.execute(100000);
+        Run<SIZE>::Result result = r.execute(1000000);
         if (result.type == Run<SIZE>::ResultType::error) {
-            std::cout << "Field could not be evaluated: " << std::endl;
-            f.print();
-            error_fields.push_back(f);
+//            std::cout << "Field could not be evaluated: " << std::endl;
+//            f.print();
+//            error_fields.push_back(f);
+            ++num_error_fields;
         }
         else if ( result.type == Run<SIZE>::ResultType::finite && result.steps > max_steps ) {
             std::cout << "Found new best with total steps: " << result.steps << std::endl;
@@ -88,7 +90,7 @@ int main()
     unsigned int duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
     std::cout << SIZE << "x" << SIZE << std::endl;
     std::cout << "Evalution took " << duration_ms/1000 << " seconds, " << duration_ms%1000 << " milliseconds" << std::endl;
-    std::cout << "There were " << error_fields.size() << " fields with failed evaluation" << std::endl;
+    std::cout << "There were " << num_error_fields << " fields with failed evaluation" << std::endl;
 
     printf("Number of fields: %ld, maximum number of steps: %d\n", iter, max_steps);
     best_field.print();
